5e30f3e43edfb7e2fd2e4fa00b9dc12fbe97c10b
Torben <mail@sauce-code.com>
2017-05-17 16:10:19 +0200

    zeugs

---
+++ src/Reader.cpp
@@ -0,0 +1,21 @@
+/*
+ * Reader.cpp
+ *
+ *  Created on: 17.05.2017
+ *      Author: Torben Krüger
+ */
+
+#include "Reader.h"
+
+namespace std {
+
+Reader::Reader() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Reader::~Reader() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ src/Reader.h
@@ -0,0 +1,21 @@
+/*
+ * Reader.h
+ *
+ *  Created on: 17.05.2017
+ *      Author: Torben Krüger
+ */
+
+#ifndef READER_H_
+#define READER_H_
+
+namespace std {
+
+class Reader {
+public:
+	Reader();
+	virtual ~Reader();
+};
+
+} /* namespace std */
+
+#endif /* READER_H_ */
