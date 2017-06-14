9791f4e26fd0a4e8a90fbf6aace4906c704582dd
Torben <mail@sauce-code.com>
2017-06-14 17:40:29 +0200

    noch schlankerer reader

--- src/Reader.cpp
+++ src/Reader.cpp
@@ -1,64 +1,21 @@
 #include "Reader.h"
 
-#include <string>
-#include <stdlib.h> // strtol
-
 Reader::Reader(const char* path) {
-//	file = open(path, O_RDONLY);
-	this->path = path;
 	file = fopen(path, "r");
 }
 
 Reader::~Reader() {
-//	close(file);
 	fclose(file);
 }
 
 int* Reader::read() {
-//	char buffer[100000];
-//	fgets(buffer, sizeof(buffer), file);
-//
-//	char** strings = new char*[COUNT];
-//
-//	char* p = strtok(buffer, " ");
-//	int count = 0;
-//	while (p) {
-////	    printf ("Token: %s\n", p);
-//	    strings[count] = p;
-//	    p = strtok(NULL, " ");
-//	    count++;
-//	}
-//
-//	long int* result = new long int[COUNT];
-//
-//	for (int i = 0; i < COUNT; i++) {
-//		result[i] = strtol(strings[i], NULL, 10);
-//	}
-//
-//	delete strings;
-//
-//	return result;
-
 	int* sequence = new int[COUNT];
 	int i = 0;
 	int counter = 0;
-	while (!feof(file) && counter < 1023) {
+	while (!feof(file) && counter < COUNT) {
 		fscanf(file, "%d", &i);
 		sequence[counter] = i;
 		counter++;
 	}
 	return sequence;
 }
-
-//void readFile(int * fileNumber, char * fileName){
-//    int i = 0;
-//    int counter = 0;
-//    FILE* fptr = fopen (fileName, "r");
-//    while (!feof (fptr) && counter < 1023) {
-//        fscanf (fptr, "%d", &i);
-//        fileNumber[counter] = i;
-//        counter++;
-//    }
-//    fclose (fptr);
-//}
-
--- src/Reader.h
+++ src/Reader.h
@@ -1,16 +1,12 @@
 #ifndef READER_H_
 #define READER_H_
 
-//#include <stdlib.h>
-//#include <fcntl.h>
-//#include <unistd.h>
 #include <stdio.h>
 
 #define COUNT 1023
 
 class Reader {
 private:
-	const char* path;
 	FILE* file;
 public:
 	Reader(const char*);
