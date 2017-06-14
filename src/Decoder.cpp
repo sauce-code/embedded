3a7352df8970ca6f9dededc84ecaf6c68b40638f
Torben <mail@sauce-code.com>
2017-06-14 18:24:49 +0200

    fuege chipsequenz array hinzu

--- src/Decoder.cpp
+++ src/Decoder.cpp
@@ -28,6 +28,34 @@ char* Decoder::decode() {
 	printf("\n");
 	ChipSequence sequence3 = ChipSequence(9, 10);
 	sequence3.printSequence();
+	ChipSequence sequence3 = ChipSequence(9, 10);
+
+	ChipSequence** sequences = new ChipSequence[SATELLITE_COUNT];
+	sequences[0] = new ChipSequence(2, 6);
+	sequences[1] = new ChipSequence(3, 7);
+	sequences[2] = new ChipSequence(4, 8);
+	sequences[3] = new ChipSequence(5, 9);
+	sequences[4] = new ChipSequence(1, 9);
+	sequences[5] = new ChipSequence(2, 10);
+	sequences[6] = new ChipSequence(1, 8);
+	sequences[7] = new ChipSequence(2, 9);
+	sequences[8] = new ChipSequence(3, 10);
+	sequences[9] = new ChipSequence(2, 3);
+	sequences[10] = new ChipSequence(3, 4);
+	sequences[11] = new ChipSequence(5, 6);
+	sequences[12] = new ChipSequence(6, 7);
+	sequences[13] = new ChipSequence(7, 8);
+	sequences[14] = new ChipSequence(8, 9);
+	sequences[15] = new ChipSequence(9, 10);
+	sequences[16] = new ChipSequence(1, 4);
+	sequences[17] = new ChipSequence(2, 5);
+	sequences[18] = new ChipSequence(3, 6);
+	sequences[19] = new ChipSequence(4, 7);
+	sequences[20] = new ChipSequence(5, 8);
+	sequences[21] = new ChipSequence(6, 9);
+	sequences[22] = new ChipSequence(1, 3);
+	sequences[23] = new ChipSequence(4, 6);
+
 
 	printf("\n");
 
